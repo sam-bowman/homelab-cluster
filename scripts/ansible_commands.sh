cbada14239aecadb44fcd6719da4eafcd2c6da86
Sam Bowman <sambowman@paypoint.com>
2024-02-01 20:09:21 +0000

    Updated group names for Master & Nodes

--- scripts/ansible_commands.sh
+++ scripts/ansible_commands.sh
@@ -1,5 +1,7 @@
 python3 -m ansible inventory -i inventory.yaml --list
 
 python3 -m ansible adhoc K3S_CLUSTER -m ping -i inventory.yaml
+python3 -m ansible adhoc K3S_MASTER -m ping -i inventory.yaml
+python3 -m ansible adhoc K3S_NODES -m ping -i inventory.yaml
 
 python3 -m ansible playbook -i inventory.yaml playbook-ping.yaml
