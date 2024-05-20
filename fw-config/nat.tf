0b0a788fc4046b7412678eae9fb93a931f882822
Hoshiwara <onishiwatsu@gmail.com>
2024-05-20 15:25:23 +0100

    test nat

---
+++ fw-config/nat.tf
@@ -0,0 +1,19 @@
+resource "opnsense_firewall_nat" "WEBSITE" {
+  enabled = false
+
+  interface = "V3000_WAN"
+  protocol  = "TCP"
+
+  destination = {
+    net  = "W3000_WAN address"
+    port = "443"
+  }
+
+  target = {
+    ip = opnsense_unbound_host_override.TRAEFIK.server
+    port = "443"
+  }
+
+  log         = true
+  description = "WEBSITE-NAT"
+}
