c3de182cdb8920b001e37ed1b6b2279f6e8efc24
Hoshiwara <onishiwatsu@gmail.com>
2024-09-15 17:37:42 +0100

    added more files to script and updating apps

--- scripts/helm-check-for-updates/app.py
+++ scripts/helm-check-for-updates/app.py
@@ -2,7 +2,14 @@ import yaml
 import subprocess
 
 files_to_check = [
-    "../argocd/utility/app-cert-manager.yaml"
+    "../argocd/utility/app-cert-manager.yaml",
+    "../argocd/utility/app-csi-secrets-store.yaml",
+    "../argocd/utility/app-github-arc.yaml",
+    "../argocd/utility/app-consul.yaml",
+    "../argocd/utility/app-vault.yaml",
+    "../argocd/utility/app-homer.yaml",
+    "../argocd/utility/app-kubernetes-dashboard.yaml",
+    "../argocd/network/app-unifi.yaml"
 ]
 
 if __name__ == "__main__":
@@ -24,7 +31,7 @@ if __name__ == "__main__":
                     repo_list.pop(0)
                     for repo in repo_list:
                         name, chart_ver, app_ver, desc = repo.split(maxsplit=3)
-                        if name == f'{chart}/{chart}':
+                        if name == f'{chart}/{chart}' and targetRevision != chart_ver:
                             print(f'New version of \'{chart}\' available')
                             print(f'    Current Version:    {targetRevision}')
                             print(f'    New Version:        {chart_ver}')
