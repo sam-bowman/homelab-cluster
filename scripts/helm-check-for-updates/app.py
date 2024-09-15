89fd8cfb39c1adae481f539e362a5f6a6640746c
Hoshiwara <onishiwatsu@gmail.com>
2024-09-15 18:39:12 +0100

    tidy up script and vm update

--- scripts/helm-check-for-updates/app.py
+++ scripts/helm-check-for-updates/app.py
@@ -22,34 +22,73 @@ files_to_check = [
     "../argocd/monitoring/app-victoria-metrics.yaml"
 ]
 
+def get_app_configuration(file):
+    with open(file, 'r') as f:
+        return yaml.safe_load(f)
+
+def get_app_sources(app_config):
+    return app_config['spec']['sources']
+
+def get_helm_chart_source(app_sources):
+    source_index = 0
+    for source in app_sources:
+        if 'chart' in source:
+            return source, source_index
+        source_index += 1
+
+def add_helm_repo(chart, repo_url):
+    return subprocess.run(['helm','repo','add',chart,repo_url], 
+                    stdout = subprocess.PIPE, 
+                    stderr = subprocess.PIPE, 
+                    universal_newlines = True).stdout
+
+def search_helm_repo(chart):
+    return subprocess.run(['helm','search','repo',chart,'--max-col-width','1000'], 
+                            stdout = subprocess.PIPE, 
+                            stderr = subprocess.PIPE, 
+                            universal_newlines = True).stdout
+
+def get_relevant_repo(repo_search, chart):
+    repo_list = repo_search.splitlines()
+    repo_list.pop(0)
+    for repo in repo_list:
+        name, chart_ver, app_ver, desc = repo.split(maxsplit=3)
+        if name == f'{chart}/{chart}':
+            return name, chart_ver, app_ver, desc
+        
+def update_available(current_version, chart_version):
+    if current_version != chart_version:
+        print(f'New version of \'{chart}\' available')
+        print(f'    Current Version:    {current_version}')
+        print(f'    New Version:        {chart_ver}')
+        return True
+    return False
+
+def apply_change(app_config, source_index, file):
+    apply_change = input(f'Apply new version to \'{file}\' ? [y/n]').lower()
+    if apply_change == 'y' or apply_change == 'yes':
+        app_config['spec']['sources'][source_index]['targetRevision'] = chart_ver
+        with open(file, 'w') as w:
+            yaml.dump(app_config, w)
+    else:
+        print(f'Not applying change \n\n')
+
 if __name__ == "__main__":
     for file in files_to_check:
-        with open(file, 'r') as f:
-            app = yaml.safe_load(f)
-            source_counter = 0
-            for source in app['spec']['sources']:
-                if 'chart' in source:
-                    chart = source['chart']
-                    repoURL = source['repoURL']
-                    targetRevision = source['targetRevision']
-                    subprocess.run(['helm','repo','add',chart,repoURL], stdout = subprocess.DEVNULL)
-                    repo_search = subprocess.run(['helm','search','repo',chart], 
-                                                 stdout = subprocess.PIPE, 
-                                                 stderr = subprocess.PIPE, 
-                                                 universal_newlines = True)
-                    repo_list = repo_search.stdout.splitlines()
-                    repo_list.pop(0)
-                    for repo in repo_list:
-                        name, chart_ver, app_ver, desc = repo.split(maxsplit=3)
-                        if name == f'{chart}/{chart}' and targetRevision != chart_ver:
-                            print(f'New version of \'{chart}\' available')
-                            print(f'    Current Version:    {targetRevision}')
-                            print(f'    New Version:        {chart_ver}')
-                            apply_change = input(f'Apply new version to \'{file}\' ? [y/n]').lower()
-                            if apply_change == 'y' or apply_change == 'yes':
-                                app['spec']['sources'][source_counter]['targetRevision'] = chart_ver
-                                with open(file, 'w') as w:
-                                    yaml.dump(app, w)
-                            else:
-                                print(f'Not applying change \n\n')
-                source_counter += 1
+
+        app_config = get_app_configuration(file)
+        app_sources = get_app_sources(app_config)
+        helm_chart_source, source_index = get_helm_chart_source(app_sources)
+
+        chart = helm_chart_source['chart']
+        repo_url = helm_chart_source['repoURL']
+        current_version = helm_chart_source['targetRevision']
+
+        add_helm_repo(chart, repo_url)
+        repo_search = search_helm_repo(chart)
+        print(repo_search)
+        print(f'\n\n')
+        chart_name, chart_ver, app_ver, chart_desc = get_relevant_repo(repo_search, chart)
+
+        if update_available(current_version, chart_ver):
+            apply_change(app_config, source_index, file)
