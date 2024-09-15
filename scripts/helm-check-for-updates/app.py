7ea4c32ee251a7c1dd930c6dfeac2a8f54afa893
Hoshiwara <onishiwatsu@gmail.com>
2024-09-15 20:07:44 +0100

    tidy up and PEP8

--- scripts/helm-check-for-updates/app.py
+++ scripts/helm-check-for-updates/app.py
@@ -1,94 +1,121 @@
-# Simple script to check all helm chart version in specified files, and update said  version if a new version
-# has been released. 
+""" Simple script to check all helm chart version in specified files,
+and update said version if a new version has been released. """
 
-import yaml
 import subprocess
 from pathlib import Path
+import yaml
 
-def kind_application(file):
-    print(f'DEBUG:      Checking file {file} to see if it is kind: Application')
-    with open(file, 'r') as f:
+
+def kind_application(resource_definition_file):
+    """Takes a yaml file, and checks the k8s resource definiton type is application"""
+    print(f'DEBUG:      Checking file {resource_definition_file} to see if it is kind: Application')
+    with open(resource_definition_file, 'r', encoding='UTF-8') as f:
         try:
             contents = yaml.safe_load(f)
-        except:
-            print(f'WARNING:    File {file} contains multiple yaml streams, skipping.')
+        except yaml.composer.ComposerError:
+            print(f'WARNING:    File {resource_definition_file} contains multiple yaml streams, skipping.')
             return False
-    if contents != None:
+    if contents is not None:
         if 'kind' in contents:
             if contents['kind'] == 'Application':
                 return True
     return False
 
+
 def get_files_to_check(base_dir):
+    """Takes a directory, and finds all yaml files inside that direcotry recursively"""
     app_files = []
     all_yamls = list(Path(base_dir).rglob("*.[yY][aA][mM][lL]"))
-    for file in all_yamls:
-        if kind_application(str(file)):
-            app_files.append(str(file))
+    for yaml_file in all_yamls:
+        if kind_application(str(yaml_file)):
+            app_files.append(str(yaml_file))
     return(app_files)
 
-def get_app_configuration(file):
-    with open(file, 'r') as f:
+
+def get_app_configuration(app_config_file):
+    """Takes a yaml configuration file, and returns the content"""
+    with open(app_config_file, 'r', encoding='UTF-8') as f:
         return yaml.safe_load(f)
 
-def get_app_sources(app_config):
+
+def get_app_sources(app_configuration):
+    """Takes an arcd app configuration, and returns the sources defined within it"""
     try:
-        return app_config['spec']['sources']
-    except:
+        return app_configuration['spec']['sources']
+    except KeyError:
         print('INFO:       No app sources found.')
         return None
 
-def get_helm_chart_source(app_sources):
-    source_index = 0
-    for source in app_sources:
+
+def get_helm_chart_source(sources):
+    """Checks a list of sources for a helm chart source defintion"""
+    idx = 0
+    for source in sources:
         if 'chart' in source:
-            return source, source_index
-        source_index += 1
-    print(f'INFO:       No helm chart source found.')
+            return source, idx
+        idx += 1
+    print('INFO:       No helm chart source found.')
     return None, None
 
-def add_helm_repo(chart, repo_url):
-    return subprocess.run(['helm','repo','add',chart,repo_url], 
-                    stdout = subprocess.PIPE, 
-                    stderr = subprocess.PIPE, 
-                    universal_newlines = True).stdout
+
+def add_helm_repo(chart, repo):
+    """Runs the 'helm repo add' command to add the repo to the local system"""
+    return subprocess.run(
+            ['helm','repo','add',chart,repo],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+            universal_newlines = True,
+            check = True).stdout
+
 
 def search_helm_repo(chart):
-    return subprocess.run(['helm','search','repo',chart,'--max-col-width','1000'], 
-                            stdout = subprocess.PIPE, 
-                            stderr = subprocess.PIPE, 
-                            universal_newlines = True).stdout
+    """Run the 'helm search repo' command for the specified chart"""
+    return subprocess.run(
+            ['helm','search','repo',chart,'--max-col-width','1000'],
+            stdout = subprocess.PIPE, 
+            stderr = subprocess.PIPE, 
+            universal_newlines = True,
+            check = True).stdout
+
 
 def get_relevant_repo(repo_search, chart):
+    """From the results of the repo search, find the one for the specified chart"""
     repo_list = repo_search.splitlines()
     repo_list.pop(0)
     for repo in repo_list:
-        name, chart_ver, app_ver, desc = repo.split(maxsplit=3)
+        name, c_ver, a_ver, desc = repo.split(maxsplit=3)
         if name == f'{chart}/{chart}':
-            return name, chart_ver, app_ver, desc
-        
-def update_available(current_version, chart_version):
-    if current_version != chart_version:
-        print(f'INFO:       New version of \'{chart}\' available')
-        print(f'INFO:           Current Version:    {current_version}')
-        print(f'INFO:           New Version:        {chart_ver}')
-        return True
-    else:
-        print(f'INFO:       Chart \'{chart}\' is already up to date!')
-        return False
+            return name, c_ver, a_ver, desc
+    return None
 
-def apply_change(app_config, source_index, file):
-    apply_change = input(f'Apply new version to \'{file}\' ? [y/n]').lower()
-    if apply_change == 'y' or apply_change == 'yes':
-        app_config['spec']['sources'][source_index]['targetRevision'] = chart_ver
-        with open(file, 'w') as w:
-            yaml.dump(app_config, w)
-        print(f'INFO:       Helm chart targetRevision updated for {app_config["spec"]["sources"][source_index]["chart"]} \n\n')
+
+def update_available(chart, current, desired):
+    """Compares the current version and the desired version of a chart"""
+    if current != desired:
+        print(f'INFO:       New version of \'{chart}\' available')
+        print(f'INFO:           Current Version:    {current}')
+        print(f'INFO:           New Version:        {desired}')
+        return True
+    print(f'INFO:       Chart \'{chart}\' is already up to date!')
+    return False
+
+
+def apply_change(config, idx, app_file, desired_version):
+    """Updates the yaml file with the new helm chart version"""
+    apply = input(f'Apply new version to \'{app_file}\' ? [y/n]').lower()
+    if apply == 'y' or apply == 'yes':
+        config['spec']['sources'][idx]['targetRevision'] = desired_version
+        with open(app_file, 'w', encoding = 'UTF-8') as w:
+            yaml.dump(config, w)
+        print(f'INFO:       Helm chart targetRevision updated for \
+{config["spec"]["sources"][idx]["chart"]} \n\n')
     else:
-        print(f'INFO:       Not applying change \n\n')
+        print('INFO:       Not applying change \n\n')
+
 
 if __name__ == "__main__":
-    print('DEBUG:      Getting list of yaml files with \'app\' in the name (should be application definition files) \n\n')
+    print('DEBUG:      Getting list of yaml files with \'app\' in the name (should be \
+application definition files) \n\n')
     files_to_check = get_files_to_check('../argocd/')
 
     for file in files_to_check:
@@ -99,23 +126,26 @@ if __name__ == "__main__":
         app_name = app_config['metadata']['name']
         print(f'DEBUG:      Getting app sources for {app_name} in file {file}')
         app_sources = get_app_sources(app_config)
-        if app_sources == None:
+        if app_sources is None:
             print(f'Skipping {app_name} in file {file}')
             continue
 
-        print(f'DEBUG:      Checking for helm chart configuration for app {app_name} in file {file}')
+        print(f'DEBUG:      Checking for helm chart configuration for app {app_name} in \
+file {file}')
         helm_chart_source, source_index = get_helm_chart_source(app_sources)
-        if helm_chart_source == None:
+        if helm_chart_source is None:
             print(f'Skipping {app_name} in file {file}')
             continue
 
-        chart = helm_chart_source['chart']
+        helm_chart = helm_chart_source['chart']
         repo_url = helm_chart_source['repoURL']
         current_version = helm_chart_source['targetRevision']
 
-        add_helm_repo(chart, repo_url)
-        repo_search = search_helm_repo(chart)
-        chart_name, chart_ver, app_ver, chart_desc = get_relevant_repo(repo_search, chart)
+        add_helm_repo(helm_chart, repo_url)
+        helm_repo_search = search_helm_repo(helm_chart)
+        chart_name, chart_ver, app_ver, chart_desc = get_relevant_repo(
+                                                        helm_repo_search,
+                                                        helm_chart)
 
-        if update_available(current_version, chart_ver):
-            apply_change(app_config, source_index, file)
+        if update_available(helm_chart, current_version, chart_ver):
+            apply_change(app_config, source_index, file, chart_ver)
