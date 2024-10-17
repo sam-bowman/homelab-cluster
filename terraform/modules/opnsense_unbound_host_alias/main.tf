b510aa235d43349117ad47151db5ce252e14ff0a
Sam Bowman <sambowman@paypoint.com>
2024-10-17 17:49:39 +0100

    whitespace

--- terraform/modules/opnsense_unbound_host_alias/main.tf
+++ terraform/modules/opnsense_unbound_host_alias/main.tf
@@ -1,7 +1,7 @@
 resource "opnsense_unbound_host_alias" "host_alias" {
-  override        = var.host_override_id
-  enabled         = var.enabled
-  description     = var.description
-  hostname        = var.hostname
-  domain          = var.domain
+  override          = var.host_override_id
+  enabled           = var.enabled
+  description       = var.description
+  hostname          = var.hostname
+  domain            = var.domain
 }
--- terraform/modules/opnsense_unbound_host_override/main.tf
+++ terraform/modules/opnsense_unbound_host_override/main.tf
@@ -1,7 +1,7 @@
 resource "opnsense_unbound_host_override" "host_override" {
-  enabled         = var.enabled
-  description     = var.description
-  hostname        = var.hostname
-  domain          = var.domain
-  server          = var.ip
+  enabled           = var.enabled
+  description       = var.description
+  hostname          = var.hostname
+  domain            = var.domain
+  server            = var.ip
 }
