f59fa77dbc80922fd9b7a3411e02d0094faae3a8
Sam Bowman <sambowman@paypoint.com>
2024-10-17 18:17:46 +0100

    modifications for use in override module

--- terraform/modules/opnsense_unbound_host_alias/main.tf
+++ terraform/modules/opnsense_unbound_host_alias/main.tf
@@ -1,7 +1,7 @@
 resource "opnsense_unbound_host_alias" "host_alias" {
   override          = var.host_override_id
   enabled           = var.enabled
-  description       = var.description
+  description       = var.description != null ? var.description : var.hostname
   hostname          = var.hostname
   domain            = var.domain
 }
--- terraform/modules/opnsense_unbound_host_alias/variables.tf
+++ terraform/modules/opnsense_unbound_host_alias/variables.tf
@@ -1,7 +1,6 @@
 variable "host_override_id" {
   description     = "(Required ; Bool) ID of the host override this alias is attached to."
   type            = string
-  default         = true
 }
 
 variable "enabled" {
@@ -13,17 +12,15 @@ variable "enabled" {
 variable "description" {
   description     = "(Optional ; Bool ; Default = Same as hostname) Description of the host alias."
   type            = string
-  default         = true
+  default         = null
 }
 
 variable "hostname" {
   description     = "(Required ; Bool) Hostname for the host alias without the domain name."
   type            = string
-  default         = true
 }
 
 variable "domain" {
   description     = "(Required ; Bool) Domain for the host alias."
   type            = string
-  default         = true
 }
