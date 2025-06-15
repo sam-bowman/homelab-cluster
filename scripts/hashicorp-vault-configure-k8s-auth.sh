11f541ae4a270882dbd51f1c7ceb9009a3bff514
Sam Bowman <sam@bowmans.site>
2025-06-15 03:51:03 +0100

    vault notes

--- scripts/hashicorp-vault-configure-k8s-auth.sh
+++ scripts/hashicorp-vault-configure-k8s-auth.sh
@@ -13,3 +13,7 @@ vault write auth/kubernetes/config kubernetes_host=https://$KUBERNETES_SERVICE_H
 vault write auth/kubernetes/config token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 vault write auth/kubernetes/role/main-role bound_service_account_names=* bound_service_account_namespaces=* policies=read-policy ttl=1h
+
+echo "Create Secret Engine named 'secret'"
+echo "Create 3 Secrets: APP_API_KEYS, cloudflare, github"
+echo "Reference SPCs for info on each"
