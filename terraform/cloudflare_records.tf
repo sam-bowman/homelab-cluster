210597cbacc815eb6d12dc96bf2fbf5f5dd39ba0
Sam Bowman <sam@bowmans.site>
2025-07-02 23:25:52 +0100

    retry

--- terraform/cloudflare_records.tf
+++ terraform/cloudflare_records.tf
@@ -5,7 +5,7 @@ data "cloudflare_zone" "bowmans_site" {
 resource "cloudflare_dns_record" "record" {
   for_each        = { for record_key, record in local.records : record_key => record if record.cf_enabled }
   zone_id         = data.cloudflare_zone.bowmans_site.zone_id
-  name            = each.value.hostname
+  name            = "${each.value.hostname}.${data.cloudflare_zone.bowmans_site.name}"
   type            = "A"
   comment         = try(each.value.description, each.value.hostname)
   content         = each.value.public_ip
@@ -15,7 +15,7 @@ resource "cloudflare_dns_record" "record" {
 resource "cloudflare_dns_record" "alias" {
   for_each        = { for alias_key, alias in local.aliases : alias_key => alias if alias.cf_enabled }
   zone_id         = data.cloudflare_zone.bowmans_site.zone_id
-  name            = each.value.hostname
+  name            = "${each.value.hostname}.${data.cloudflare_zone.bowmans_site.name}"
   type            = "CNAME"
   comment         = try(each.value.description, each.value.hostname)
   content         = "${cloudflare_dns_record.record[each.value.alias_of].name}.${data.cloudflare_zone.bowmans_site.name}"
