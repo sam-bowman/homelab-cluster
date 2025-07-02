81954b0a043e10b867f6c8718f5e5c57d04146dd
Sam Bowman <sam@bowmans.site>
2025-07-02 23:37:09 +0100

    only 1 level of subdomain for the free certs

--- terraform/cloudflare_records.tf
+++ terraform/cloudflare_records.tf
@@ -5,7 +5,7 @@ data "cloudflare_zone" "bowmans_site" {
 resource "cloudflare_dns_record" "record" {
   for_each        = { for record_key, record in local.records : record_key => record if record.cf_enabled }
   zone_id         = data.cloudflare_zone.bowmans_site.zone_id
-  name            = "${lower(each.value.hostname)}"
+  name            = "${replace(lower(each.value.hostname),".","-")}"
   type            = "A"
   comment         = try(each.value.description, each.value.hostname)
   content         = each.value.public_ip
@@ -15,10 +15,10 @@ resource "cloudflare_dns_record" "record" {
 resource "cloudflare_dns_record" "alias" {
   for_each        = { for alias_key, alias in local.aliases : alias_key => alias if alias.cf_enabled }
   zone_id         = data.cloudflare_zone.bowmans_site.zone_id
-  name            = "${lower(each.value.hostname)}"
+  name            = "${replace(lower(each.value.hostname),".","-")}"
   type            = "CNAME"
   comment         = try(each.value.description, each.value.hostname)
-  content         = "${lower(cloudflare_dns_record.record[each.value.alias_of].name)}.${data.cloudflare_zone.bowmans_site.name}"
+  content         = "${replace(lower(cloudflare_dns_record.record[each.value.alias_of].name),".","-")}.${data.cloudflare_zone.bowmans_site.name}"
   proxied         = each.value.cf_proxy_enabled
   ttl             = 1
 }
