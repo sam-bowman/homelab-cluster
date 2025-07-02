c9f3e204e4e3167f81983db19411beac103ff100
Sam Bowman <sam@bowmans.site>
2025-07-02 21:39:03 +0100

    zone_id

--- terraform/cloudflare_records.tf
+++ terraform/cloudflare_records.tf
@@ -4,7 +4,7 @@ data "cloudflare_zone" "bowmans_site" {
 
 resource "cloudflare_dns_record" "record" {
   for_each        = { for record_key, record in local.records : record_key => record if record.cf_enabled }
-  zone_id         = data.cloudflare_zone.zone_id
+  zone_id         = data.cloudflare_zone.bowmans_site.zone_id
   name            = each.value.hostname
   type            = "A"
   comment         = try(each.value.description, each.value.hostname)
@@ -13,7 +13,7 @@ resource "cloudflare_dns_record" "record" {
 }
 resource "cloudflare_dns_record" "alias" {
   for_each        = { for alias_key, alias in local.aliases : alias_key => alias if alias.cf_enabled }
-  zone_id         = data.cloudflare_zone.zone_id
+  zone_id         = data.cloudflare_zone.bowmans_site.zone_id
   name            = each.value.hostname
   type            = "CNAME"
   comment         = try(each.value.description, each.value.hostname)
