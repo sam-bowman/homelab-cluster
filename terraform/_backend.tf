c4953c544bc7caa8b4db1c4b14f536178adea03b
Sam Bowman <sam@bowmans.site>
2025-07-03 00:15:22 +0100

    address of consul

--- terraform/_backend.tf
+++ terraform/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "consul" {
-    address = "consul.homelab.bowmans.site"
+    address = "consul-homelab.bowmans.site"
     scheme  = "https"
     path    = "terraform/fw-config-state.tfstate"
   }
